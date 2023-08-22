60d719a7331fad73e0215e518e61c0634a60a013
SOARESGG10 <rafael.soares.developer@gmail.com>
2023-08-22 10:20:45 -0300

    📑| Alteração do nome da tabela para 'recipe_ingredients'

---
+++ database/migrations/2023_08_17_004832_create_recipe_ingredients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes_ingredients', function (Blueprint $table) {
+
+            $table->unsignedInteger("ingredient_id")->comment("Referência ao identificador único do ingrediente");
+            $table->foreign("ingredient_id")->references("id")->on("ingredients")->onDelete("cascade")->onUpdate("cascade");
+
+            $table->unsignedBigInteger("revenue_id")->comment("Referência ao identificador único da receita");
+            $table->foreign("revenue_id")->references("id")->on("revenues")->onDelete("cascade")->onUpdate("cascade");
+
+            $table->unsignedSmallInteger("measure_id")->nullable()->comment("Referência ao identificador único da medida da receita");
+            $table->foreign("measure_id")->references("id")->on("measures")->onDelete("set null")->onUpdate("cascade");
+
+            $table->smallInteger('amount')->comment("Quantidade do ingrediente na receita");
+
+            $table->primary(["ingredient_id", "revenue_id"]);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes_ingredients');
+    }
+};
