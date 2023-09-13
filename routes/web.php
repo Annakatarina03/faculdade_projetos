b750b57c28de9c789f0b8777c4e8974fa0c8db87
Anna katarina <katarinanna3c@gmail.com>
2023-09-13 10:00:15 -0300

    arrumando a rota da welcome

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
 });
+*/
 
 Route::get('/dashboard', function () {
     return view('dashboard');
