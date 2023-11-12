84a2a30c093fc8d5dd09bbab7b052d41073513d5
devrafaelsoares <rafael.soares.developer@gmail.com>
2023-11-12 14:14:03 -0300

    delete: Deleção de componentes desnecessários

--- resources/views/senac/recipe-tasting/my-tasting/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-@section('title', 'Receitas | Minhas degustação')
-@section('content')
-    @livewire('recipe-tasting.my-tasting.index')
-    @livewire('component.modal')
-@endsection
--- resources/views/senac/recipe-tasting/schedule-tasting/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-@section('title', 'Receitas | Marcar degustação')
-@section('content')
-    @livewire('recipe-tasting.schedule-tasting.index')
-    @livewire('component.modal')
-@endsection
