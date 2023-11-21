b6f02bdd99b609e233a4423e3c064d5e4bae16f3
Anna katarina <katarinanna3c@gmail.com>
2023-11-21 00:26:45 -0300

    subindo a minha parte

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,20 +1,70 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            <form action="{{ route('logout') }}" method="POST" action="">
-                @csrf
-                <button type="submit">Sair</button>
-            </form>
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="pt-BR">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css'])
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <section class="min-h-[95.5vh] flex flex-col items-center justify-center bg-[#F4F1E9]">
+        <div class="w-screen md:w-auto flex flex-col bg-white py-20 md:px-20 rounded-xl shadow-[4px_2px_3px_rgba(0,0,0,0.3)]">
+            <div class="flex items-center">
+                <div>
+                    <h1 class="text-xl md:text-3xl font-bold text-[#D1D9DF] ">
+                        Sistema de livro de acervo de receitas
+                    </h1>
+                </div>
+                <div class="pl-8">
+                    <img src="{{ asset('images/logo-system.png') }}" alt="S" class="max-w-[50px] md:max-w-[100px]">
                 </div>
             </div>
+
+            <div class="d-flex justify-content-between align-items-center" style="font-size: 18px;">
+                <div class="flex items-center">
+                    <div class="pr-8">
+                        <h2><strong>Bem-Vindo, {{ Auth::user()->name }} !</strong></h2>
+                        <br>
+                        <p><strong>Seu cargo é de: {{ Auth::user()->office->name }}</strong></p>
+                        <br>
+                        <p>Estamos muito felizes que esteja aqui,<br>contribuindo e engajando com o nosso sistema<br>de acervo de livro de receitas.</p>
+                    </div>
+                    <div>
+                        <img src="{{ asset('images/dashboard.png') }}" alt="P" class="max-w-[200px] md:max-w-[200px]">
+                    </div>
+                </div>
+                <br>
+                <button type="button"  class="btn btn-secondary custom-btn" onclick="redirectToNewRoute()">Prosseguir</button>
+            </div>
         </div>
-    </div>
-</x-app-layout>
+
+    </section>
+    <footer class="text-center text-white fixed bottom-0 w-screen bg-[#2A384C]">
+        <div class="text-center p-3">
+            © 2023 Copyright:
+            <a class="text-white" href="#">Senac ADS</a>
+        </div>
+    </footer>
+</body>
+</html>
+
+<style>
+    .custom-btn {
+        background-color: #6c757d;
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 20px;
+        cursor: pointer;
+    }
+</style>
+
+
+<script>
+     function redirectToNewRoute() {
+        window.location.href = '/admin/employees';
+    }
+</script>
